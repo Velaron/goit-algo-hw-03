8c3ac9af2c9541d9ca108e862bc152024d37d598
Bohdan Shulyar <mars873@gmail.com>
2025-06-14 10:52:44 +0300

    Update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_numbers_ticket(min_num: int, max_num: int, quantity: int) -> list[int]:
     if min_num < 1 or max_num > 1000:
         return []
 
-    if quantity < min_num or quantity > max_num:
+    if quantity > max_num - min_num + 1:
         return []
 
     numbers = random.sample(range(min_num, max_num + 1), k=quantity)
